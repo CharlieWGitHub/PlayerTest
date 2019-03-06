31082cd768aa99ec45452d5a7f4c721db86feef3
wangchenglong <wangchenglong@sdjictec.com>
2019-03-06 14:45:47 +0800

    [Feature]添加播放库
    Change-Id: I5eb53b4fb48be525f3bac742c011c649310b702e

---
+++ AVPlayer/AVPlayer/Classes/System/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by 王成龙 on 2019/3/6.
+//  Copyright © 2019 Charlie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ AVPlayer/AVPlayer/Classes/UI/Controllers/AVPlayerViewController.h
@@ -0,0 +1,17 @@
+//
+//  AVPlayerViewController.h
+//  AVPlayer
+//
+//  Created by 王成龙 on 2019/3/6.
+//  Copyright © 2019 Charlie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AVPlayerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
